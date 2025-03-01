94bff21be15cca2dcfdd79c65cda2ea118022c8d
afthab <mohammedafthab15@gmail.com>
2025-03-01 22:05:59 +0530

    removed env file and completed the project

--- handler/teacher_handler.go
+++ handler/teacher_handler.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"qrgen/service/model"
 	"qrgen/service/service"
@@ -322,5 +325,60 @@ func (h *hanlderLayer) ViewAllAttendacne(c *gin.Context) {
 
 	c.JSON(200, gin.H{
 		"attendees": attendanceData.Attendees,
+	})
+}
+
+func (h *hanlderLayer) ViewStudentToTeacher(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	_, ok := ctx.Value("claimskey").(string)
+	if !ok {
+		log.Error().Msg("traceid missing from context")
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": http.StatusText(http.StatusInternalServerError),
+		})
+		return
+	}
+
+	studentID := c.Query("id")
+
+	// Convert the string studentID to MongoDB ObjectID (if it's ObjectID)
+	objID, err := primitive.ObjectIDFromHex(studentID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid student ID"})
+		return
+	}
+
+	var studentDetail model.Student
+
+	// Step 2: Initialize MongoDB connection and collection
+	collection := h.dbconn.Collection("students")
+
+	// Find the student in the MongoDB collection
+	err = collection.FindOne(context.TODO(), bson.M{"_id": objID}).Decode(&studentDetail)
+	if err == mongo.ErrNoDocuments {
+		// If no document found
+		c.JSON(http.StatusNotFound, gin.H{"message": "Student not found"})
+		return
+	} else if err != nil {
+		// Handle other potential errors
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error finding student"})
+		return
+	}
+
+	// Load the image from local folder and encode it in base64
+	imagePath := fmt.Sprintf("./qr_codes/%s.png", studentID)
+	imageData, err := ioutil.ReadFile(imagePath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error reading student image"})
+		return
+	}
+
+	// Encode image to base64
+	studentDetail.Image = base64.StdEncoding.EncodeToString(imageData)
+
+	// Return the student as JSON
+	c.JSON(http.StatusOK, gin.H{
+		"student detail": studentDetail,
 	})
 }
